c869ed51905ed2c9185b72bd0f24f75212eae95c
appstrategy <eprleads@gmail.com>
2013-04-11 23:10:50 -0700

    created initial flow in main.js (updated name), updated html file with correct student info

---
+++ main.js
@@ -0,0 +1,21 @@
+// alert("JavaScript works!");
+
+// Peter Hitchcock
+// SDI 1304
+// April 11, 2013
+// Project 2
+
+// Start
+
+// Initial Variables Sting, Number, Array
+
+// Procedure
+
+// Boolean Function
+        
+// Number Function & While Loop
+
+// String Function
+        
+// Array Function & For Loop
+        
