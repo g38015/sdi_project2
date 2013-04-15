b5dd4cec6335b97f2e741e52f88b3c18f0c2865b
appstrategy <eprleads@gmail.com>
2013-04-15 11:57:42 -0700

    started all 5 functions added syntax for while and for loops

--- main.js
+++ main.js
@@ -8,17 +8,55 @@
 // Start
 
 // Initial Variables Sting, Number, Array
-var str = ""
-var num = 0
-var ar = [1,2,3]
 
-// Procedure
+var skiRuns = [ "Beginner", "Intermediate", "Advanced"],
+		skiLifts = [ 1, 2, 3];
+var resortName = "Sierra";
+var sierra = 'Sierra\'s runs are still epic!';
+var snowInches = 10;
+var newSnow = true;
 
-// Boolean Function
-        
-// Number Function & While Loop
 
-// String Function
+/* Procedure
+One function that doesn't return a value (a procedure)
+*/
+var skiResort = function () {};
+
+
+/* Boolean Function
+One function that takes two arguments and does some kind of Boolean comparison between them. The data type of the arguments doesn't matter for this function
+*/
+var boo = function () {};
         
-// Array Function & For Loop
+/* Number Function & While Loop
+One function that takes a single number and uses it in a while loop somehow, doing some math at the same time. This can be something simple like counting or summing, but again you are encouraged to look into more interesting applications
+*/
+var num = function () {
+		while () {
+
+				if () {
+
+				};
+
+				if () {
+
+
+				};
+
+		};
+};
+
+/* String Function
+One function that does something interesting with strings after accepting two strings as arguments. This can be simple concatenation, but you are encouraged to look into other, more interesting string applications
+*/
+var str = function () {};
         
+/* Array Function & For Loop
+One function that takes an array and a number and uses them in a for loop of some kind. This should not be a simple index dereference (looking up one value by its index). Make it something more interesting
+*/
+var arr = function () {
+		for () {
+
+		};
+};
+		
