d472a093ab141650046a8a93b730088052b3e77a
appstrategy <eprleads@gmail.com>
2013-04-16 10:06:19 -0700

    updated math function now working

--- main.js
+++ main.js
@@ -9,54 +9,41 @@
 
 // Initial Variables Sting, Number, Array
 
-var skiRuns = [ "Beginner", "Intermediate", "Advanced"],
-		skiLifts = [ 1, 2, 3];
-var resortName = "Sierra";
-var sierra = 'Sierra\'s runs are still epic!';
-var snowInches = 10;
-var newSnow = true;
+
+var mathFunction = function(snowToday, snowYesterday){
+		var totalSnow = snowToday + snowYesterday;
+		console.log("We have a total of " + totalSnow + " inches of snow to ski on");
+};
+mathFunction(10,5);
+
 
 
 /* Procedure
 One function that doesn't return a value (a procedure)
-*/
-var skiResort = function () {};
+
+var skiResort = function () {
+	if (true) {};
+
+};
 
 
 /* Boolean Function
 One function that takes two arguments and does some kind of Boolean comparison between them. The data type of the arguments doesn't matter for this function
-*/
+
 var boo = function () {};
         
-/* Number Function & While Loop
-One function that takes a single number and uses it in a while loop somehow, doing some math at the same time. This can be something simple like counting or summing, but again you are encouraged to look into more interesting applications
-*/
-var num = function () {
-		while () {
-
-				if () {
-
-				};
-
-				if () {
-
-
-				};
-
-		};
-};
 
 /* String Function
 One function that does something interesting with strings after accepting two strings as arguments. This can be simple concatenation, but you are encouraged to look into other, more interesting string applications
-*/
+
 var str = function () {};
         
 /* Array Function & For Loop
 One function that takes an array and a number and uses them in a for loop of some kind. This should not be a simple index dereference (looking up one value by its index). Make it something more interesting
-*/
+
 var arr = function () {
 		for () {
 
 		};
 };
-		
+*/	
