56d9ac9d4c34a107e0c2019822009385bfa20964
appstrategy <eprleads@gmail.com>
2013-04-17 09:36:27 -0700

    changed variables in proceed function

--- main.js
+++ main.js
@@ -10,20 +10,21 @@
 // Initial Variables Sting, Number, Array
 var str = "String",
 		num = 10,
-		arr = [];
+		arr = [],
+		newSnow = true;
 
 // Procedure Function
-var proceed = function(arg1){
-		if (arg1 === true)
+var didItSnow = function(checkForSnow){
+		
+		if (checkForSnow === true)
 		{
-			console.log("Go Back");
+			console.log("It Snowed");
 		}
 		else
 		{
-			console.log("Return");
+			console.log("No Snow");
 		}
 };
-proceed(1<2);
 
 // Boolean Function
 var boolFunction = function(arg1, arg2){
@@ -36,26 +37,29 @@ var boolFunction = function(arg1, arg2){
 	  	console.log("I am false");
 	  }
 };
-boolFunction((1>2) && (2<3))
 
 // Number Function
 var mathFunction = function(snowToday, snowYesterday){
 		var totalSnow = snowToday + snowYesterday;
 		console.log("We have a total of " + totalSnow + " inches of snow to ski on");
 };
-mathFunction(10,5);
 
 // String Function
 var stringFunction = function(stringOne, stringTwo){
 		var stringConcat = stringOne + " " + stringTwo;
 		console.log(stringConcat);
 };
-stringFunction("Yeah", "Killer")
 
 // Array Function
 var arrayFunction = function(num, ar) {
 		var arr = [];
 		console.log("here is the number " + num + " here is the array " + ar);
 };
-arrayFunction(10, [1,2,3])
+
+// Main 
+didItSnow(newSnow);
+boolFunction((1>2) && (2<3))
+mathFunction(10,5);
+stringFunction("Yeah", "Killer")
+arrayFunction(10, [1,2,3,4])
               
